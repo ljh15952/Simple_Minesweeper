919e615a8e4a6f9128705a5107c1e58a3ecd74cc
중화이 <ljh15952@gmail.com>
2023-06-22 15:08:01 +0000

    Click event

--- src/ms.js
+++ src/ms.js
@@ -1,42 +1,45 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d')
-
+let canvas = document.getElementById('canvas'); let ctx = canvas.getContext('2d')
+ctx.font = "15px Arial";
 let pos = {x: -1, y: -1};
-
-let board = {num: Array(16).fill(0).map(()=>Array(30).fill(0)),
-			info: Array(16).fill(0).map(()=>Array(30).fill(0))};
-
+let board = {num: Array(16).fill(0).map(()=>Array(30).fill(0)),info: Array(16).fill(0).map(()=>Array(30).fill(0))};
 let mx = [-1,-1,-1,0,0,1,1,1];
 let my = [-1,0,1,-1,1,-1,0,1];
 
-let isclicked = false;
-
 canvas.addEventListener('mousemove', e => {
+	e.preventDefault();
 	const canv = canvas.getBoundingClientRect();
 	pos = {x: Math.floor((e.clientX - canv.left) / 18), y: Math.floor((e.clientY - canv.top) / 18) };
 })
 
 canvas.addEventListener('click', () => {
-	board.info[pos.y][pos.x] = 1;
-	drawBoard();
+	if(board.info[pos.y][pos.x] !== 2){
+		board.info[pos.y][pos.x] = 1;
+		drawBoard();
+	}
 })
 
-ctx.font = "15px Arial";
+canvas.addEventListener('contextmenu', e => {
+	e.preventDefault();
+	board.info[pos.y][pos.x] = 2 - board.info[pos.y][pos.x];
+	drawBoard();
+});
+
 const drawBoard = () => {
 	ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
-	
 	board.info.map((a, i)=>{
 		a.map((b, j)=>{
+			ctx.fillStyle = 'black';
 			if(b === 1){
 				ctx.strokeRect(j * 18, i * 18, 17, 17);
 				ctx.fillText(board.num[i][j],j * 18 + 4, i * 18 + 14);	
+			}else if(b === 2){
+				ctx.fillStyle = 'red';
+				ctx.fillRect(j * 18, i * 18, 17, 17);
 			}else{
 				ctx.fillRect(j * 18, i * 18, 17, 17);
 			}
 		})
 	})
-	
-	
 }
 
 const setBoard = (y,x) =>{
