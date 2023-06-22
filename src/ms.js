89e554a0091baca2799a4f2e46e0349d7ee3bb73
중화이 <ljh15952@gmail.com>
2023-06-22 15:56:11 +0000

    Complete

--- src/ms.js
+++ src/ms.js
@@ -5,6 +5,7 @@ let board = {num: Array(16).fill(0).map(()=>Array(30).fill(0)),info: Array(16).f
 let mx = [-1,-1,-1,0,0,1,1,1];
 let my = [-1,0,1,-1,1,-1,0,1];
 
+
 canvas.addEventListener('mousemove', e => {
 	e.preventDefault();
 	const canv = canvas.getBoundingClientRect();
@@ -13,7 +14,7 @@ canvas.addEventListener('mousemove', e => {
 
 canvas.addEventListener('click', () => {
 	if(board.info[pos.y][pos.x] !== 2){
-		board.info[pos.y][pos.x] = 1;
+		dfs(pos.y, pos.x);
 		drawBoard();
 	}
 })
@@ -42,7 +43,27 @@ const drawBoard = () => {
 	})
 }
 
-const setBoard = (y,x) =>{
+const dfs = (y,x) => {
+	
+	board.info[y][x] = 1;
+	
+	if(board.num[y][x] > 0 || board.num[y][x] === '*')
+		return;
+	
+	
+	for(let i = 0; i < 8; ++i){
+		if(y + my[i] < 0 || y + my[i] >= 16 
+		|| x + mx[i] < 0 || x + mx[i] >= 30) 
+			continue;
+		if(board.info[y+my[i]][x+mx[i]] === 1)
+			continue;
+		if(board.num[y+my[i]][x+mx[i]] !== '*')
+			dfs(y+my[i], x+mx[i]);
+	}
+	
+}
+
+const setBoard = (y,x) => {
 	for(let k = 0; k < 8; ++k){
 		if(y + my[k] < 0 || y + my[k] >= 16 
 		|| x + mx[k] < 0 || x + mx[k] >= 30) 
